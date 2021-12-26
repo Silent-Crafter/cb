89842d5d97e380e896179a0b062a95451b9ef03f
Silent Crafter <nikhilsolanke5@gmail.com>
2021-12-26 16:04:30 +0530

    fix

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@
 
 mkdir -p /usr/local/bin
 
-chmod +x backlight
-chmod +x cb
+/sbin/chmod +x backlight
+/sbin/chmod +x cb
+
 cp -v ./backlight /usr/local/bin
 cp -v ./cb /usr/local/bin
 cp -v ./backlight.1.gz /usr/share/man/man1
 cp -v ./cb.1.gz /usr/share/man/man1
+cp -v ./backlight.service /usr/lib/systemd/system/
 
-systemctl enable backlight.service
-systemctl start backlight.service
+/sbin/systemctl daemon-reload
+/sbin/systemctl enable backlight.service
+/sbin/systemctl start backlight.service
 
+echo
 echo "Installation Completed"
 echo
 echo "Chack the man pages for info: cb(1) and backlight(1)"
